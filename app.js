ba424cc0c7b0d4ca3dea6371f5195ce7888379bd
spriyanka2244 <spriyanka2244@gmail.com>
2023-06-01 16:09:16 +0530

    bg ans color

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("hi")
+var header = document.getElementById('header-title');
+header.style.borderBottom ='solid 3px #000';
+
+var items =document.getElementsByClassName('list-group-item');
+items[0].style.fontWeight='bold';
+items[0].style.backgroundColor ='yellow';
+
