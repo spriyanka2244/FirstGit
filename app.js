bd832520d0bf8996654b239809301667e084cc98
spriyanka2244 <spriyanka2244@gmail.com>
2023-06-03 15:52:46 +0530

    GetElementsbyTAgName

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+//get elements by tagname
+var li =document.getElementsByTagName('li');
+li[1].textContent='Hello 5';
+li[2].style.fontWeight='bold';
+li[4].style.backgroundColor='red';
 
-//get element by class name
-var items =document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor ='green';
-
-for(var i=0;i<items.length;i++){
-   items[i].style.fontWeight='bold';
+for(var i=0;i<li.length;i++){
+    li[4].style.backgroundColor="red";
 }
 
+
+//editing by getelement by class name 
+var items =document.getElementsByClassName('list-group-item');
+for(var i=0;i<items.length;i++){
+      items[i].style.fontWeight='bold';
+  } 
+
+var items1 =document.getElementsByClassName('list-group');
+for(var i=0;i<items.length;i++){
+    items[i].style.backgroundColor='yellow';
+} 
